3f2c830893d371041b7cccb9b372f7be97f085dc
Eduard <ylane9@live.fr>
2022-05-19 20:24:14 +0200

    apple spawn random spawn added, when snake goes on an apple it had a piece to his body, and add one on score

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,8 +3,11 @@
 		<div id="grid">
 			<div class="grid-item" :id="`grid${item-1}`" v-for="item in grid_case" v-bind:key="item">
 				<img id="snake-head" v-if="data.head[item-1]" src="../assets/snake_head.svg" alt="snake-head">
+				<img id="apple" v-if="data.apple[item-1]" src="../assets/apple.svg" alt="apple">
+				<img id="body" v-if="data.body[item-1]" src="../assets/body_straight.svg" alt="body_straight">
 			</div>
 		</div>
+		<h1 id="score">Score : {{data.score}}</h1>
 	</div>
 	<!-- <input type="text" @keydown.arrow-down="moove_head(data)"> -->
 </template>
@@ -15,11 +18,34 @@
 	let grid_case = ref(50); // The size of the grid is not reactive for now
 	let data = reactive({
 		head: [],
-		head_rotation: "", 
+		body: [],
+		apple: [],
+		head_rotation: "",
+		where_body: [],
+		score: 0,
 	})
 
+	function add_body(direction) {
+		let head = go_to_head(data);
+
+		if (direction == "up") {
+			data.body[head] = 1;
+		}
+		else if (direction == "down") {
+			data.body[head] = 2;
+		}
+		else if (direction == "right") {
+			data.body[head] = 3;
+		}
+		else if (direction == "left") {
+			data.body[head] = 4;
+		}
+		data.where_body.unshift(head);
+		return ;
+	}
+
 	function turn_head() {
-		console.log(`head rotation = ${data.head_rotation}`);
+		// console.log(`head rotation = ${data.head_rotation}`);
 		// let temp = document.getElementById("snake-head").style.transform;
 		// console.log(`dId = ${temp}`);
 		if (data.head_rotation == "up") {
@@ -39,19 +65,16 @@
 	function go_to_head(data) {
 		let i = 0;
 
-		while (data.head[i] != true)
-		{
-			i++;
-		}
-
+		while (data.head[i] != true) { i++; }
 		return (i)
 	}
 
-	function create_tab(i, array){
+	function init_tab(i, array, which){
 		let j = 0;
 
 		while (j < i){
-			array.push(false);
+			if (which == "false") {array.push(false);}
+			if (which == "minus_one") {array.push(false);}
 			j++;
 		}
 	}
@@ -59,10 +82,10 @@
 	function check_if_border(nb) {
 		// Need to change these values to be reactive to the size of the map
 		if (nb % 10 == 0 || nb % 10 == 9 || Math.floor(nb / 10) == 0 || Math.floor(nb / 10) == 4) {
-			console.log(`nb = ${nb / 10} `);
+			// console.log(`nb = ${nb / 10} `);
 			return (1);
 		}
-		console.log(`nb = ${nb / 10} `);
+		// console.log(`nb = ${nb / 10} `);
 		return (0);
 	}
 
@@ -89,24 +112,96 @@
 		}
 	}
 
-	function moove_head(data, where) {
-		let j = 0;
+	function spawn_apple(data) {
+		let rand = Math.floor(Math.random() * 49);
+		let head = go_to_head(data);
 
-		while (j < data.head.length) {
-			if (data.head[j] == true)
-			{
-				if ((j % 10 == 0 && where == -1) || (j % 10 == 9 && where == 1) || (j + where >= 50) || (j + where < 0))
-				{
-					// console.log(j, where);
-					alert("GAME OVER");
-					return ;
-				}
-				data.head[j] = false;
-				data.head[j + where] = true;
-				return ;
-			}
-			j++;
+		while (rand == head || data.body[rand]){
+			let rand = Math.floor(Math.random() * 49);
 		}
+		data.apple[rand] = true;
+		// console.log(rand + data.apple[rand]);
+	}
+
+	function moove_body(data) {
+		let head = go_to_head(data);
+		if (data.where_body.length < 1)
+		{
+			return;
+		}
+
+		if (data.head_rotation == "up") {
+			data.body[head] = 1;
+		}
+		else if (data.head_rotation == "down") {
+			data.body[head] = 2;
+		}
+		else if (data.head_rotation == "right") {
+			data.body[head] = 3;
+		}
+		else if (data.head_rotation == "left") {
+			data.body[head] = 4;
+		}
+		let temp1 = data.body[data.where_body[0]];
+		let temp2 = data.where_body[0];
+
+		data.body[data.where_body[0]] = 0;
+		data.where_body[0] = head;
+
+
+		let i = 1;
+		while (i < data.where_body.length) {
+			let where_body = data.where_body[i];
+			let direction = data.body[where_body];
+
+				data.body[temp2] = temp1;
+				data.where_body[i] = temp2;
+
+			// if (temp2 == 1) {
+			// 	data.body[temp2] = temp1;
+			// 	data.where_body[i] = temp2;
+			// }
+			// else if (temp2 == 2) {
+			// 	data.body[temp2] = temp1;
+
+			// }
+			// else if (temp2 == 3) {				
+			// 	data.body[temp2] = temp1;
+
+			// }
+			// else if (temp2 == 4) {
+			// 	data.body[temp2] = temp1;
+
+			// }
+			data.body[where_body] = 0;
+			temp1 = direction;
+			temp2 = where_body;
+			i++;
+		}
+	}
+
+	function moove_head(data, where) {
+		let j = go_to_head(data);
+
+		if ((j % 10 == 0 && where == -1) || (j % 10 == 9 && where == 1) || (j + where >= 50) || (j + where < 0) || data.body[j + where])
+		{
+			// console.log(j, where);
+			alert("GAME OVER");
+			return ;
+		}
+		if (data.apple[j + where] == true) {
+			data.apple[j + where] = false;
+			data.score++;
+			add_body(data.head_rotation);
+			data.head[j + where] = true;
+			data.head[j] = false;
+			spawn_apple(data);
+			return ;
+		}
+		moove_body(data);2
+		data.head[j + where] = true;
+		data.head[j] = false;
+		return ;
 	}
 	// There is a problem, in go in to GAME OVER when the code is update
 
@@ -138,7 +233,7 @@
 			else if (data.head_rotation == "down") {moove_head(data, 10);}
 			else if (data.head_rotation == "right") {moove_head(data, 1);}
 			else {moove_head(data, -1);}
-			console.log(i);
+			// console.log(i);
 			if (i > 100) {
 				i -= Math.sqrt(Math.sqrt(i)); // Need to modifiy the time, but another formula, or by change the way I do it, because here, the less time there is, the more it do the operation, so less time 
 			}
@@ -146,8 +241,11 @@
 		}, i);
 	}
 
-	create_tab(50, data.head);
+	init_tab(50, data.head, "false");
+	init_tab(50, data.apple, "false" )
+	init_tab(50, data.body, "minus_one")
 	spawn_head(data);
+	spawn_apple(data);
 	event_listener_html();
 
 	onMounted(() => {
@@ -165,7 +263,15 @@
 		padding: 0px;
 		margin: 0px;
 	}
-
+	#game {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+	#score {
+		margin-top: 3vh;
+	}
 	#grid {
 		width: 100.25vh;
 		height: 50.25vh;
@@ -180,11 +286,19 @@
 		border: 0.25vh solid black;
 		background-color: grey;
 		display: flex;
-		justify-content: flex-start;
+		justify-content: center;
 		align-items: center;
 	}
 	#snake-head {
 		width: 10vh;
 		height: auto;
 	} 
+	#body {
+		width: 10vh;
+		height: auto;
+	}
+	#apple {
+		width: 7vh;
+		height: auto;
+	}
 </style>
